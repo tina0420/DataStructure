25991b645387fd4b470e839287e4b6c27515b336
tina0420 <hsu.tina88@yahoo.com.tw>
2020-11-24 15:05:29 +0800

    Maze(unfinished)

--- src/stack/Maze.java
+++ src/stack/Maze.java
@@ -1,10 +1,66 @@
 package stack;
 
+import java.io.*;
+
 public class Maze {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	//設定迷宮出口位置
+	public static int exitx = 9;
+	public static int exity = 10;
+	//宣告迷宮的陣列
+	public static int [][] Maze= {
+			{1,1,1,1,1,1,1,1,1,1,1,1},
+			{1,0,0,0,1,1,1,1,1,1,1,1},
+			{1,1,1,0,1,1,0,0,0,0,1,1},
+			{1,1,1,0,1,1,0,1,1,0,1,1},
+			{1,1,1,0,0,0,0,1,1,0,1,1},
+			{1,1,1,0,1,1,0,1,1,0,1,1},
+			{1,1,1,0,1,1,0,1,1,0,1,1},
+			{1,1,1,1,1,1,0,1,1,0,1,1},
+			{1,1,0,0,0,0,0,0,1,0,0,1},
+			{1,1,1,1,1,1,1,1,1,1,0,1}, //出口在這
+			{1,1,1,1,1,1,1,1,1,1,1,1}
+			};
+	
+	public static void main(String[] args) throws IOException{
+		int x, y, i, j;
+		TraceRecord path = new TraceRecord();
+		System.out.println("0的位置");
+		x = 1;
+		y = 1;
+		for (i=0; i<11; i++) {
+			for (j=0; j<12; j++) {
+				System.out.print(Maze[i][j]);
+			}
+			System.out.println();
+		}
+		while(x <= exitx && y <= exity) {
+			//走過的位置存成2
+			//1為牆壁、0是可以走的路
+			Maze[x][y] = 2;
+			
+			//上下左右邊的格子能不能走
+			if(Maze[x][y+1] == 0) {
+				y = y+1;
+				path.insert(x, y);
+			}else if(Maze[x][y-1] == 0){
+				y = y+1;
+				path.insert(x, y);
+			}else if(Maze[x-1][y] == 0) {
+				x-=x;
+				path.insert(x, y);
+			}else if(Maze[x+1][y] == 0) {
+				x+=x;
+				path.insert(x, y);
+			}
+			}
+		System.out.println("走過的路徑 : ");
+//		for (i=0; i<11; i++) {
+//			for(j=0; j<12; j++) {
+//				System.out.print(Maze[i][j]);
+//			}
+//			System.out.println();
+//		}
+		
 }
+}
+
--- src/stack/TraceRecord.java
+++ src/stack/TraceRecord.java
@@ -1,26 +1,47 @@
 package stack;
 
+class Node{
+	int x;
+	int y;
+	Node next;
+	public Node(int x,int y) {
+		this.x=x;
+		this.y=y;
+		this.next=null;
+	}
+}
+
 public class TraceRecord {
+	public Node first;
+	public Node last;
+	public boolean isEmpty() {
+		return first == null;
+	}
 	
-	class Node{
-		int x;
-		int y;
-		Node next;
-		public Node(int x,int y) {
-			this.x=x;
-			this.y=y;
-			this.next=null;
-		}
-	}
-	public class TraceRecord{
-		public Node first;
-		public Node last;
-		public boolean isEmpty() {
-			return first == null;
-		}
-	}
-	public void insert(int x,int y) {
+	//打點加入stack
+	public void insert(int x, int y) {
 		Node newNode = new Node(x,y);
-		if (this.)
+		if(this.isEmpty()) {
+			first = newNode;
+			last = newNode;
+		}else {
+			last.next = newNode;
+			last = newNode;
+		}
+	}
+
+	//把點從stack刪除
+	public void delete(int x, int y) {
+		Node newNode = new Node(x,y);
+		if(this.isEmpty()) {
+			System.out.println("stack已經空了");
+			return;
+		}
+		newNode = first;
+		while(first.next != last) {
+			newNode = newNode.next;
+		}
+		first.next = last.next;
+		last = newNode;
 	}
 }
